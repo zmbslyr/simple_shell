ff3dc9d2cac7ebfab54cc37a4974533015dce99a
SeckMohameth <mohamethseck34@gmail.com>
2019-04-17 19:05:01 +0000

    Main file

---
+++ hsh.c
@@ -0,0 +1,142 @@
+#include "shell.h"
+
+/**
+ * main - Simple Shell
+ * @argc: number of arguments
+ * @argv: Arguments stored as string
+ * @envp: Enviroment parameters
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)),
+	 char *argv[],
+	 char *envp[])
+{
+	char *buffer = NULL, *path, *pathExec, *delim1 = "\t ";
+	size_t bufSize = 0;
+	ssize_t charCount;
+	char **array = NULL;
+	/* make startup function */
+	globals.name = argv[0];
+	signal(SIGINT, sigHandle);
+	if (isatty(STDOUT_FILENO) == 1 && isatty(STDIN_FILENO) == 1)
+		flags.interactive = 1;
+	/* make startup function */
+	for (globals.count = 1; 1; globals.count++)
+	{
+		if (flags.interactive)
+			write(STDERR_FILENO, "($) ", 4);
+		charCount = getline(&buffer, &bufSize, stdin);
+		if (charCount < 0)
+			break;
+		if (buffer[charCount - 1] == '\n')
+			buffer[charCount - 1] = '\0';
+		exitShell(buffer);
+		array = vect(buffer, delim1,  charCount);
+		globals.cmd = array[0];
+		path = pathFind(envp);
+		pathExec = execPath(path, array[0]);
+		parse(array, envp);
+		if (builtins(array[0]))
+		{
+			if (pathExec != NULL)
+				newProcess(pathExec, array, envp);
+			freeArray(array);
+		}
+		else
+		{
+			free(pathExec);
+			freeArray(array);
+		}
+		free(pathExec);
+	}
+	if (charCount < 0 && flags.interactive)
+		write(STDERR_FILENO, "\n", 1);
+	free(buffer);
+	return (globals.exit);
+}
+
+/**
+ * newProcess - Creates a new process
+ * @pathExec: Executable path
+ * @args: Array of arguments given
+ * @env: Array of env variables
+ *
+ * Return: void
+ */
+void newProcess(char *pathExec, char **args, char **env)
+{
+	pid_t newProcess;
+	int status;
+
+	newProcess = fork();
+	if (newProcess < 0)
+		nfError(-1);
+	if (newProcess == 0)
+	{
+		execve(pathExec, args, env);
+		nfError(-1);
+		exit(2);
+	}
+	else
+		wait(&status);
+}
+/**
+ * builtins - checks if cmd is a built-in
+ * @cmd: Command to check
+ *
+ * Return: 0 if built-in, 1 if not
+ */
+int builtins(char *cmd)
+{
+	char *built[4] = {
+		"cd",
+		".",
+		"env",
+		NULL
+	};
+	int index;
+
+	for (index = 0; built[index] != NULL; index++)
+	{
+		if (cmd == NULL || _strcmp(cmd, built[index]) == 0)
+			return (0);
+	}
+	return (1);
+}
+/**
+ * envBuilt - Built-in: env
+ * @cmd: Command to check against env
+ * @env: Takes in env from main
+ *
+ * Return: void
+ */
+void envBuilt(char *cmd, char **env)
+{
+	char *envp = "env";
+	int index = 0;
+
+	if (_strcmp(cmd, envp) == 0)
+	{
+		while (env[index] != NULL)
+		{
+			write(1, env[index], _strlen(env[index]));
+			write(1, "\n", 1);
+			index++;
+		}
+	}
+}
+
+/**
+ * parse - parses a string
+ * @array: Token array
+ * @env: Enviroment
+ *
+ * Return: void
+ */
+void parse(char **array, char **env)
+{
+	globals.arg = array[1];
+	envBuilt(array[0], env);
+	cd(array[0], array[1]);
+}
